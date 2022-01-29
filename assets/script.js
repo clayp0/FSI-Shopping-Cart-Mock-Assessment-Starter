521aa2a1174160d8cec4aa320524497c8de2c349
clayp0 <c.anders.christenson@gmail.com>
2022-01-29 11:41:55 -0600

    finished decreaseBtn event listener

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+let decreaseBtn = document.querySelector("#quantity-down")
+let increaseBtn = document.querySelector("#quantity-up")
+
+function updateQuantity(displayQuantity){
+	let quantity = document.querySelector(".total-quantity")
+	quantity.innerHTML = displayQuantity
+}
+
+
+let quantity = 1
+
+decreaseBtn.addEventListener("click", function(e){
+	if(quantity > 0){
+		quantity--
+	} else {
+		window.alert("Nothing in the cart, dummy! (luv u)")
+	}
+	updateQuantity(`Quantity: ${quantity}`)
+})
+
